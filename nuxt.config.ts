49ea3533276dcdf080c5144ad332c1039a2bc01e
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-03-13 17:22:25 -0300

    chore: updates dependencies and configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,11 +9,6 @@ const localLayerPath = resolve(currentDir, "../rimelight-components");
 const isLocalLayer = existsSync(localLayerPath);
 
 export default defineNuxtConfig({
-  compatibilityDate: "2026-02-13",
-  future: {
-    compatibilityVersion: 5,
-  },
-
   extends: [
     [
       isLocalLayer ? localLayerPath : "github:Rimelight-Entertainment/rimelight-components",
@@ -36,8 +31,8 @@ export default defineNuxtConfig({
         "0 0 * * *": ["cleanup-notes-trash", "cleanup-todos-archived"],
       },
       routeRules: {
-        "/documents/**": { isr: 3600 },
-        "/blog/**": { isr: 3600 },
+        "/documents/**": { isr: true },
+        "/blog/**": { isr: true },
         "/dashboard/**": {
           ssr: false,
           appLayout: "dashboard",
@@ -105,6 +100,21 @@ export default defineNuxtConfig({
           content: "Queer Kit",
         },
       ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "/favicon.svg",
+        },
+        {
+          rel: "preconnect",
+          href: "https://cdn.queerkit.com",
+        },
+        {
+          rel: "dns-prefetch",
+          href: "https://cdn.queerkit.com",
+        },
+      ],
     },
   },
 
@@ -112,21 +122,31 @@ export default defineNuxtConfig({
     headers: {
       contentSecurityPolicy: {
         "img-src": ["https://cdn.queerkit.com"],
-        "connect-src": ["https://cdn.queerkit.com"],
-      },
-    },
+        "connect-src": ["https://queerkit.com"]
+      }
+    }
   },
 
   i18n: {
+    strategy: "prefix_except_default",
+    defaultLocale: "en",
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_redirected",
+      cookieSecure: true,
+      alwaysRedirect: false,
+    },
     locales: [
       {
         code: "en",
         name: "English",
+        language: "en-US",
         file: "en.json",
       },
       {
         code: "pt",
         name: "Português",
+        language: "pt-BR",
         file: "pt.json",
       },
     ],
@@ -168,6 +188,7 @@ export default defineNuxtConfig({
   },
 
   image: {
+    domains: ["queerkit.com"],
     cloudflare: {
       baseURL: "https://cdn.queerkit.com",
     },
