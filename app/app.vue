c8d65fc844f3ccc3cd976f71b4c20716f5a2c857
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-03-18 17:58:55 -0300

    fix: general standardization fixes

--- app/app.vue
+++ app/app.vue
@@ -148,6 +148,21 @@ useSeoMeta({
   twitterImage: `${appConfig.cdn}/images/logos/logomark-white.webp`,
   twitterCard: "summary_large_image"
 })
+
+/*
+if (import.meta.env.PROD) {
+  useScriptCloudflareWebAnalytics({
+    token: "8309799b4ff448d1b447a451b5d62c1c",
+    scriptInput: {
+      crossorigin: "anonymous"
+    },
+    scriptOptions: {
+      trigger: "onNuxtReady",
+      skipValidation: true
+    }
+  })
+}
+*/
 </script>
 
 <template>
